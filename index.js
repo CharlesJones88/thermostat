1b1ef4cf69ee27c8fc405829144219272b1c7ec3
Charles Jones <charlesjones1088@gmail.com>
2016-09-05 18:57:19 -0600

    Adding user config to config.json

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ var sockjs = require('sockjs');
 var zerorpc = require('zerorpc');
 var bodyParser = require('body-parser');
 var winston = require('winston');
+var content = fs.readFileSync('config');
 var logger = new (winston.Logger)({
     transports: [
-        new(winston.transports.File)({filename: '/usr/bin/thermostat/thermostat.log'})
+        new(winston.transports.File)({filename: content['logging']})
     ]
 });
 
-var status = 'https://api.particle.io/v1/devices/240020000b47353235303037/status?access_token=effedec05de7182739f864e78b5c630af947f93c';
+var status = content['statusUrl'];
 
-var pkey = fs.readFileSync('/usr/bin/thermostat/key.pem');
-var pcert = fs.readFileSync('/usr/bin/thermostat/cert.pem');
+var pkey = fs.readFileSync(content['key']);
+var pcert = fs.readFileSync(content['cert']);
 
 var options = {
     key: pkey,
@@ -24,15 +25,8 @@ var options = {
 };
 
 client = new zerorpc.Client();
-try {
-    logger.info('Starting RCP connection');
-    client.connect("tcp://127.0.0.1:4242");
-} catch(err) {
-    console.error(err);
-    setTimeout(function() {
-        client.connect("tcp://127.0.0.1:4242");
-    }, 60000);
-}
+logger.info('Starting RCP connection');
+client.connect(content['clientRPC']);
 
 client.on('error', function(error) {
     logger.error('RPC client error: ' + error);
@@ -245,9 +239,9 @@ server.post('/fan', function(req, res) {
 server.get('/state', function(req, res) {
     try {
         https.get({
-            host: 'api.particle.io',
-            port: 443,
-            path: '/v1/devices/240020000b47353235303037/status?access_token=effedec05de7182739f864e78b5c630af947f93c',
+            host: content['host'],
+            port: content['port'],
+            path: content['path'],
             method: 'GET'
         }, function(response) {
             response.on('data', function(data) {
@@ -268,8 +262,8 @@ server.get('/state', function(req, res) {
     }
 });
 
-var port = 8080;
-var sport = 8443;
+var port = content['httpListen'];
+var sport = content['httpsListen'];
 var httpServer = http.createServer(server);
 var httpsServer = https.createServer(options, server);
 httpServer.listen(port);
